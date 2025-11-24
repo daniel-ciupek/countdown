999d3fd46d430cb4628f8cd312e520d72bce6bf8
daniel-ciupek <danter005@o2.pl>
2025-11-24 19:51:04 +0100

    Adding a new Countdown instance in CountdownManager

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ init() {
     this.countdownCounter = 0;
 
     this.countdownsContainer = document.querySelector(".countdowns-container");
+    this.countdowns = [];
 
     this.disableTickerPastDate();
     this.addListeners();
@@ -34,6 +35,40 @@ addListeners = () => {
     });
 }
 addNewCountdown = ({title, date, container}) => {
+const countdownId = "countdown" + this.countdownCounter++;
+
+const code = `<div class="countdown" id="${countdownId}">
+    <h3 class="countdown-title">${title}</h3>
+    <div class="countdown-body">
+        <ul class="timer">
+            <li><span class="days"></span>Dni</li>
+            <li><span class="hours"></span>Godzin</li>
+            <li><span class="minutes"></span>Minut</li>
+            <li><span class="seconds"></span>Sekund</li>
+        </ul>
+        <div class="countdown-finished-info">Odliczanie zakończone!</div>
+        <button class="countdown-delete">X</button>
+    </div>
+</div>`;
+
+let element = document.createElement("div");
+element.innerHTML = code;
+element = element.firstChild;
+container.appendChild(element);
+
+const countdown = new Countdown({
+    title: title, date: date,
+    id: countdownId
+});
+
+countdown.addDeleteListener(() => {
+this.removeCountdownFromHtml(countdown);
+});
+
+this.countdowns.push(countdown);
+}
+
+removeCountdownFromHtml = (c) => {
 
 }
 
